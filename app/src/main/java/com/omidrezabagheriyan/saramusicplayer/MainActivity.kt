b1208bc961ff0a2b80852d8584fe8176f351a64a
OmidrezaBagheriyan <OmidrezaBagheriyan@gmail.com>
2023-12-19 01:54:12 +0330

    Config ViewBinding in MainActivity.kt

--- app/src/main/java/com/omidrezabagheriyan/saramusicplayer/MainActivity.kt
+++ app/src/main/java/com/omidrezabagheriyan/saramusicplayer/MainActivity.kt
@@ -2,10 +2,17 @@ package com.omidrezabagheriyan.saramusicplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.omidrezabagheriyan.saramusicplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(mainBinding.root)
     }
 }
